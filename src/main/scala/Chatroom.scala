6135326909981705b73c1751289852557cdc2c0f
PhyrexTsai <x01ep23i@hotmail.com>
2016-02-24 17:57:25 +0800

    change new Client(args) => classOf[Client], args; it is a better way for using

--- src/main/scala/Chatroom.scala
+++ src/main/scala/Chatroom.scala
@@ -17,24 +17,24 @@ object Chatroom {
     val server2 = system.actorOf(Props(new Server("Chat 2")))
 
     // create client
-    val c1 = system.actorOf(Props(new Client("Sam", server)))
+    val c1 = system.actorOf(Props(classOf[Client], "Sam", server))
 
     c1 ! Send("HI")
 
-    val c2 = system.actorOf(Props(new Client("Helen", server)))
+    val c2 = system.actorOf(Props(classOf[Client], "Helen", server))
 
     c2 ! Send("Hello")
 
-    val c3 = system.actorOf(Props(new Client("John", server)))
+    val c3 = system.actorOf(Props(classOf[Client], "John", server))
 
     c3 ! Send("Hello!!!")
 
     // Disconnect
     c3 ! Disconnect
 
-    val c4 = system.actorOf(Props(new Client("Paul", server2)))
+    val c4 = system.actorOf(Props(classOf[Client], "Paul", server2))
 
-    val c5 = system.actorOf(Props(new Client("Mary", server2)))
+    val c5 = system.actorOf(Props(classOf[Client], "Mary", server2))
 
     c4 ! Send("New guy")
 
